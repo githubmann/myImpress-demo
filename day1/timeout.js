a36f039a5f6c5be3b8ebf8606e4c049c4f6ff32b
howtosay111 <1041763635@qq.com>
2016-05-05 12:19:15 +0800

    setTimeout 只执行一次

---
+++ day1/timeout.js
@@ -0,0 +1,17 @@
+/*
+* @Author: howtosay111
+* @Date:   2016-05-05 12:18:09
+* @Last Modified by:   howtosay111
+* @Last Modified time: 2016-05-05 12:18:22
+*/
+
+
+
+'use strict';
+var timeDate = 'sdfa';
+var timer = function (timeDate) {
+    setTimeout(function () {
+        console.log(timeDate);
+    }, 10);
+};
+timer(timeDate);
