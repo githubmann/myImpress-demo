b11d953083089fe174ef9a63efd016ebcab49f56
howtosay111 <1041763635@qq.com>
2016-05-05 12:59:19 +0800

    setTimeout的回调

--- day1/timeout.js
+++ day1/timeout.js
@@ -2,7 +2,7 @@
 * @Author: howtosay111
 * @Date:   2016-05-05 12:18:09
 * @Last Modified by:   howtosay111
-* @Last Modified time: 2016-05-05 12:44:39
+* @Last Modified time: 2016-05-05 12:58:39
 */
 
 
@@ -24,6 +24,9 @@ function timer(data) {
         return console.log('结束');
     }
     //每个1秒调用一次这个函数--自身
-    setTimeout(timer(data), 10000);
+    //不要在setTimeout调用函数如timer(data)
+    setTimeout(timer,1000);
 }
 timer(timeDate);
+
+// timedCount();
