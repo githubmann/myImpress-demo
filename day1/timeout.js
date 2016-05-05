f1756de7e14e2b2eeab6aff37a76146d0e672873
howtosay111 <1041763635@qq.com>
2016-05-05 12:45:33 +0800

    当执行时，时间会马上跳完

--- day1/timeout.js
+++ day1/timeout.js
@@ -2,18 +2,28 @@
 * @Author: howtosay111
 * @Date:   2016-05-05 12:18:09
 * @Last Modified by:   howtosay111
-* @Last Modified time: 2016-05-05 12:27:33
+* @Last Modified time: 2016-05-05 12:44:39
 */
 
 
 
 'use strict';
 var timeDate = 10000;
+function parseTime(data) {
+    var hours = parseInt(data/(1000*60*60));
+    var minutes = parseInt((data%(1000*60*60))/(1000*60));
+    var seconds = parseInt((data%(1000*60)/1000));
+    return {hours,minutes,seconds};
+}
 //定义一个函数为timer--计时器
 function timer(data) {
-    console.log('倒计时  ',data);
-    data--;
+    var rightTime = parseTime(data);
+    console.log('倒计时  ',rightTime);
+    data = data -1000;
+    if(data<0){
+        return console.log('结束');
+    }
     //每个1秒调用一次这个函数--自身
-    setTimeout(timer(data), 1000);
+    setTimeout(timer(data), 10000);
 }
 timer(timeDate);
