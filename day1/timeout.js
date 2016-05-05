2eaa67d153985d600eca57ee1c0df980c8792fd0
howtosay111 <1041763635@qq.com>
2016-05-05 12:28:21 +0800

    用setTimeout实现无穷循环

--- day1/timeout.js
+++ day1/timeout.js
@@ -2,16 +2,18 @@
 * @Author: howtosay111
 * @Date:   2016-05-05 12:18:09
 * @Last Modified by:   howtosay111
-* @Last Modified time: 2016-05-05 12:18:22
+* @Last Modified time: 2016-05-05 12:27:33
 */
 
 
 
 'use strict';
-var timeDate = 'sdfa';
-var timer = function (timeDate) {
-    setTimeout(function () {
-        console.log(timeDate);
-    }, 10);
-};
+var timeDate = 10000;
+//定义一个函数为timer--计时器
+function timer(data) {
+    console.log('倒计时  ',data);
+    data--;
+    //每个1秒调用一次这个函数--自身
+    setTimeout(timer(data), 1000);
+}
 timer(timeDate);
